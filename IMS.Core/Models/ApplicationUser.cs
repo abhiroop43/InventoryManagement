a42ecb0716a5f7bc7e947c422cee662e5f864dee
Abhiroop Santra <abhiroop.santra@gmail.com>
2026-02-23 19:47:54 +0400

    feat(auth): :sparkles: add user claims retrieval in InventoriesList
    * Inject `IHttpContextAccessor` to access user claims.
    * Display user information (`FullName`, `PreferredUsername`, `Uid`) on the inventory page.
    * Update `OnInitializedAsync` to fetch and store claims for better personalization.
    * Minor formatting adjustments to improve code readability.
    
    This enhancement improves the user experience by showing personalized user details.
    
    Signed-off-by: Abhiroop Santra <abhiroop.santra@gmail.com>

---
+++ IMS.Core/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+namespace IMS.Core.Models;
+
+public class ApplicationUser
+{
+    public string? FullName { get; set; }
+    public string? Email { get; set; }
+    public string? Uid { get; set; }
+}
